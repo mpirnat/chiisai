4e359ca7fba9dc2cd5ec736aad384ff445d2c47e
Mike Pirnat <mpirnat@gmail.com>
2012-10-05 23:15:43 -0400

    Adds some docstrings to the shortener

--- chiisai/shortener.py
+++ chiisai/shortener.py
@@ -1,22 +1,28 @@
-"""Shortener stuff"""
+"""Core shortener logic"""
 
 from hashlib import md5
 
 
 def make_hash(value):
+    """Make a url-friendly md5 hash of a value."""
     md_5 = md5()
     md_5.update(value)
+
+    # get the hashed value, being careful to strip out characters
+    # that are not url-safe
     hashed = md_5.digest().encode('base64')\
             .replace('\n','')\
             .rstrip('=')\
             .replace('/', '')\
             .replace('+', '')
+
     return hashed
 
 
-bad_words = ['fuck', 'shit', 'piss', 'cock', 'bitch', 'cunt']
+bad_words = ['fuck', 'shit', 'piss', 'cock', 'cunt', 'tits', 'bitch']
 
 def is_clean(value):
+    """Is the value devoid of 'unsavory' language?"""
     value = value.lower()
     for bad_word in bad_words:
         if bad_word in value:
