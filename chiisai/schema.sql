f9d1196872a6c3d8b3bdbee293083ab5109d1627
Mike Pirnat <mpirnat@gmail.com>
2012-10-06 00:18:38 -0400

    Adds basic database schema definition

---
+++ chiisai/schema.sql
@@ -0,0 +1,8 @@
+drop table if exists urls;
+create table urls (
+    id integer primary key autoincrement,
+    hash text unique not null,
+    url text not null,
+    created text not null, -- ISO8601 date string: "YYYY-MM-DD HH:MM:SS.SSS"
+    hits integer
+);
