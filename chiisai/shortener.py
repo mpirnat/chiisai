ee70b3e3000ea61de0b33705b0d4aff30b340de9
Mike Pirnat <mpirnat@gmail.com>
2012-10-05 23:00:44 -0400

    Adds basic 'bad words' checking

--- chiisai/shortener.py
+++ chiisai/shortener.py
@@ -6,5 +6,19 @@ from hashlib import md5
 def make_hash(value):
     md_5 = md5()
     md_5.update(value)
-    hashed = md_5.digest().encode('base64').replace('\n','').rstrip('=')
+    hashed = md_5.digest().encode('base64')\
+            .replace('\n','')\
+            .rstrip('=')\
+            .replace('/', '')\
+            .replace('+', '')
     return hashed
+
+
+bad_words = ['fuck', 'shit', 'piss', 'cock', 'bitch', 'cunt']
+
+def is_clean(value):
+    value = value.lower()
+    for bad_word in bad_words:
+        if bad_word in value:
+            return False
+    return True
--- tests/test_shortener.py
+++ tests/test_shortener.py
@@ -6,10 +6,39 @@ from chiisai import shortener
 
 class TestWhenMakingHash(object):
 
-    def setup(self):
-        self.url = 'http://example.com'
-        self.hashed = shortener.make_hash(self.url)
-
     def test_it_makes_a_hash(self):
+        value = 'http://example.com'
         expected_hash = 'qbnwQzbOAYGgjndOARE7MQ'
-        assert_equals(self.hashed, expected_hash)
+
+        hashed = shortener.make_hash(value)
+
+        assert_equals(hashed, expected_hash)
+
+    def test_it_makes_a_urlsafe_hash(self):
+        value = 'foobaristan'
+
+        hashed = shortener.make_hash(value)
+
+        assert '+' not in hashed, hashed
+        assert '/' not in hashed, hashed
+
+        assert '?' not in hashed, hashed
+        assert '#' not in hashed, hashed
+        assert '=' not in hashed, hashed
+        assert '&' not in hashed, hashed
+
+
+class TestCensoringHash(object):
+
+    def test_it_passes_innocuous_strings(self):
+        hashed = 'puppies and rainbows'
+        assert shortener.is_clean(hashed)
+
+    def test_it_fails_censorable_strings(self):
+        hashed = 'fuck12qDSffagwa467MQagaRE7'
+        assert not shortener.is_clean(hashed)
+
+    def test_it_finds_mixed_case_bad_words(self):
+        hashed = 'fUcK12qDSffagwa467MQagaRE7'
+        assert not shortener.is_clean(hashed)
+
